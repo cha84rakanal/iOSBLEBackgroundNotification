75b19b46bf6f20ac65a810331e1587a550f0cb11
Tatsuya Minagawa <cha84rakanal@gmail.com>
2021-12-03 07:33:03 +0900

    project init

---
+++ iOSBLEBackgroundNotification/iOSBLEBackgroundNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSBLEBackgroundNotification
+//
+//  Created by Minagawa Tatsuya on 2021/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
